01a6daea23aa0a190b6029f5eb715274236f1d62
Niel <danielescobar492@gmail.com>
2026-02-17 22:37:55 -0500

    added color pallette

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div className="bg-gray-950 flex flex-col items-center justify-center h-screen">
+    <div className="bg-neutralui-800 flex flex-col items-center justify-center h-screen">
       <p className="text-2xl font-bold text-white">App New!!</p>
     </div>
   );
