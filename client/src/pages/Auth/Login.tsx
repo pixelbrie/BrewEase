9888cdf5edf03d2c5f96b6ae66ed59b6a1f85692
Niel <danielescobar492@gmail.com>
2026-02-23 16:24:17 -0500

    added agent and customer navigation on login

---
+++ client/src/pages/Auth/Login.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import { FaEnvelope, FaLock } from "react-icons/fa";
+import brewEaseLogo from "../../assets/images/BrewEaseLogoTrans.png";
+import backgroundLogin from "../../assets/images/backgroundLogin.png";
+
+function Login() {
+  //Variables to hold form data and current page state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [pin, setPin] = useState(["", "", "", ""]);
+  const [currentPage, setCurrentPage] = useState("customer");
+
+  const handlePageChange = () => {
+    // Toggle between customer and agent login pages
+    setCurrentPage(currentPage === "customer" ? "agent" : "customer");
+  };
+
+  useEffect(() => {
+    // Reset form data when page changes
+    setEmail("");
+    setPassword("");
+    setPin(["", "", "", ""]);
+  }, [currentPage]);
+
+  const LoginContainer = () => {
+    return (
+      <div className="flex flex-col gap-7 items-center justify-center w-full">
+        <p className="text-lg text-center text-neutral-400">
+          Please enter your details to join your account
+        </p>
+        <Input placeholder="Email" type="email" icon={<FaEnvelope />} />
+        <Input placeholder="Password" type="password" icon={<FaLock />} />
+        <Button
+          label="Login"
+          className="w-64 justify-center items-center bg-coffee-700 text-white font-bold py-4 px-8 rounded-full"
+          variant="secondary"
+          size="large"
+          onClick={() => console.log("Login button clicked")}
+        />
+        <span className="text-sm text-neutral-400">
+          Don't have an account?{" "}
+          <a href="#" className="text-coffee-600 font-bold">
+            Sign Up
+          </a>
+        </span>
+        <div className="flex flex-row gap-2 w-full items-center">
+          <span className="bg-coffee-200 h-0.5 w-full border-neutralui-100"></span>
+          <p className="text-lg text-center text-neutral-400">or</p>
+          <span className="bg-coffee-200 h-0.5 w-full border-neutralui-100"></span>
+        </div>
+        <Button
+          label="Continue as Guest"
+          className="w-64 justify-center items-center bg-coffee-700 text-white font-bold py-4 px-8 rounded-full"
+          variant="secondary"
+          size="large"
+          onClick={() => console.log("Login button clicked")}
+        />
+      </div>
+    );
+  };
+
+  const AgentLoginContainer = () => {
+    return (
+      <div className="flex flex-col gap-7 items-center justify-center w-full">
+        <p className="text-lg text-center text-neutral-400">
+          Please enter your details to join your account
+        </p>
+        <div className="flex flex-row"></div>
+      </div>
+    );
+  };
+  return (
+    <div
+      className="flex 1 flex-col text-coffee-800 items-center justify-center h-screen"
+      style={{
+        backgroundImage: `url(${backgroundLogin})`,
+        backgroundSize: "cover",
+      }}
+    >
+      {/* Login Form */}
+      <div className="font-nunito flex flex-col lg:w-[500px] sm:w-[500px] xs:w-[300px] gap-7 items-center justify-center p-20 rounded-xl bg-neutral-100">
+        <img
+          src={brewEaseLogo}
+          alt="BrewEase Logo"
+          className="w-28 h-28 mb-4"
+        />
+
+        <p className="text-3xl font-bold">
+          {currentPage === "customer" ? "Customer Login" : "Agent Login"}
+        </p>
+
+        {currentPage === "customer" ? (
+          <LoginContainer />
+        ) : (
+          <AgentLoginContainer />
+        )}
+
+        <div className="flex flex-row gap-4 w-full items-center">
+          <span className="bg-coffee-200 h-0.5 w-full border-neutralui-100"></span>
+          <a
+            href="#"
+            className="text-coffee-600 font-bold whitespace-nowrap"
+            onClick={handlePageChange}
+          >
+            {currentPage === "customer" ? "Agent Login" : "Customer Login"}
+          </a>
+          <span className="bg-coffee-200 h-0.5 w-full border-neutralui-100"></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
