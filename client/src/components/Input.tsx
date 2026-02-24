81fd5b7e607aa9f66bad4e5a41eb747cfd9a8f46
Niel <danielescobar492@gmail.com>
2026-02-24 15:15:19 -0500

    fixed input problems

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -5,6 +5,9 @@ interface InputProps {
   placeholder?: string;
   type?: string;
   classname?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  length?: number;
 }
 
 /**
@@ -13,10 +16,18 @@ interface InputProps {
  * @param {string} [placeholder] - The placeholder text for the input
  * @param {string} [type] - The type of the input
  * @param {string} [classname] - Additional class names for the input
+ * @param {number} [length] - The maximum length of the input
  * @returns {JSX.Element} The rendered Input component
  */
 
-function Input({ icon, placeholder, type, classname }: InputProps) {
+function Input({
+  icon,
+  placeholder,
+  type,
+  classname,
+  value,
+  onChange,
+}: InputProps) {
   return (
     <div className="flex flex-col w-full">
       <div className="relative flex items-center w-full">
@@ -28,6 +39,9 @@ function Input({ icon, placeholder, type, classname }: InputProps) {
         <input
           type={type || "text"}
           placeholder={placeholder || "Enter text"}
+          value={value}
+          onChange={onChange}
+          maxLength={length}
           className={`text-black ${icon ? "pl-10" : "px-4"} py-4 w-full rounded-lg border-2 focus:outline-none border-gray-200 focus:border-3 focus:ring-coffee-600 focus:border-coffee-600 focus:ring-2 focus:ring-opacity-50 transition duration-300 ease-in-out ${classname || ""}`}
         />
       </div>
