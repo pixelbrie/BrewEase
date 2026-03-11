0668ddcb2ee0337345a643d382cb38e3082ad81c
ChrisSanchezDev <ChrisSanchezDev@gmail.com>
2026-03-11 17:05:16 -0400

    wip: firestore initialization

---
+++ server/src/config/validateDB.js
@@ -0,0 +1,16 @@
+import { db } from './config/firebaseAdmin.js';
+
+async function validateDB() {
+  try {
+    console.log("Checking Cloud Firestore DB connection...");
+    await db.collection('dothiswork_questionmark').doc('idk').get();
+    console.log("Firestore connection successful!")
+    return true;
+  }
+  catch (error) {
+    console.error("Firestore connection failed!");
+    console.log(error.message)
+    console.error("Ending program early due to DB failure.");
+    process.exit(1)
+  }
+}
