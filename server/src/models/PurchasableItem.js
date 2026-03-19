d977573e7f32b8abd1424f1d1f5459ac22730913
ChrisSanchezDev <chrissanchezdev@gmail.com>
2026-03-19 14:05:58 -0400

    feat: finished models

---
+++ server/src/models/PurchasableItem.js
@@ -0,0 +1,14 @@
+export default class PurchasableItem {
+  constructor(itemId, itemName, categoryId, basePrice, description=null, previewImage=null, sizes=[], flavors=[], available=true, taxable=true) {
+    this.itemId = itemId;
+    this.itemName = itemName;
+    this.categoryId = categoryId;
+    this.description = description;
+    this.previewImage = previewImage;
+    this.sizes = sizes;
+    this.flavors = flavors;
+    this.basePrice = basePrice;
+    this.available = available;
+    this.taxable = taxable;
+  }
+}
