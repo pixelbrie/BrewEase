7dfa16703fa8356ba6eb52ebcec49eb413f3609f
ChrisSanchezDev <chrissanchezdev@gmail.com>
2026-03-24 02:22:47 -0400

    feat: completed models + seed.js + teardown.js

---
+++ server/src/models/Category.js
@@ -0,0 +1,8 @@
+export default class Category {
+  constructor({ categoryId, name, orderIndex, active=true }) {
+    this.categoryId = categoryId;
+    this.name = name;
+    this.orderIndex = orderIndex;
+    this.active = active;
+  }
+}
