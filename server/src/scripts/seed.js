05669985ad2b96089d9baa7d96d4a799aa310ffa
ChrisSanchezDev <chrissanchezdev@gmail.com>
2026-03-24 01:56:57 -0400

    feat: teardown.js + slightly updated seed.js

--- server/src/scripts/seed.js
+++ server/src/scripts/seed.js
@@ -1,8 +1,10 @@
-// TODO: Prolly add this into a specific scripts folder since it's something you activate on it's own.
+// seed.js: Spot where you manually create fake test data. When done with the tests, you may use teardown.js
+// TODO: Add counter like you did in teardown.js
 import { db } from '../config/firebaseAdmin.js';
 import { Role, Employee } from '../models/index.js'; // TODO: Add customer stuff too
 
 async function seedDatabase (){
+  console.log("Starting the seed.js process...")
   const batch = db.batch();
 
   // Creating test role
@@ -14,6 +16,7 @@ async function seedDatabase (){
   });
   const roleRef = db.collection('roles').doc(newRole.roleId);
   batch.set(roleRef, { ...newRole }); // The ... is basically writing the roleId: "", roleName: "", etc., it just writes out what you already have in newRole.
+  console.log(`${newRole.roleId} added to roles.`)
 
   // TODO: Create test employee
   // Fields: (employeeId, firstName, lastName, email, phone, roleId, pinHash, active=true)
@@ -28,10 +31,11 @@ async function seedDatabase (){
   });
   const employeeRef = db.collection('employees').doc(newEmployee.employeeId);
   batch.set(employeeRef, { ...newEmployee });
+  console.log(`${newEmployee.employeeId} added to employees.`)
 
   try {
     await batch.commit(); // Sends all the queued writes together. If one operation fails, the whole batch fails (to help avoid partial writing.)
-    console.log('BrewEase seed data successfully added.');
+    console.log('All seed data successfully added!');
     process.exit(0);
   } catch (error) {
     console.error('Error seeding database: ', error);
